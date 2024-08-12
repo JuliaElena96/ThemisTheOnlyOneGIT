3f5b0263261d51e635f7c65b2bc153282b74502d
Elena <elena.prinz96@gmail.com>
2024-08-12 16:36:39 +0200

    Kundenmaske weiter
    NavBar zu Full Page Tab geändert und Kundenmaske.html weiter programmiert

--- js/navbar.js
+++ js/navbar.js
@@ -1,26 +1,15 @@
-function einblenden() {
-    document.getElementById("suchcontainer").style.visibility = "visible";
-    $("#suchcontainer").show();
-    document.getElementById("addCustomer").style.visibility = "visible";
-    $(".addCustomer").show();
 
-}
-function ausblenden() {
-    $(".welcome").hide();
-
-}
 function einblendenKundentabelle() {
     document.getElementById("kundentabelle").style.visibility = "visible";
     $("#kundentabelle").show();
 }
 
-function ausblendenFuerGesellschaft() {
-    $("#suchcontainer").hide();
-    $(".addCustomer").hide();
-    $("#kundentabelle").hide();
-}
-
-function einblendenGesellschaft(){
-    document.getElementById("gesellschaft").style.visibility = "visible";
-    $("#gesellschaft").show();
-}
+$(document).ready(function() {
+    $('.tab-button').click(function() {
+        var tabId = $(this).data('tab');
+        $('.tab-button').removeClass('active');
+        $(this).addClass('active');
+        $('.tab').removeClass('active');
+        $('#' + tabId).addClass('active');
+    });
+});
